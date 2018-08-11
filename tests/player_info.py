27293076c62afc374faec40699b3379971ca499f
johnvictorfs <johnvictorfs@gmail.com>
2018-08-11 19:41:43 -0300

    Changed strings on test prints to use f-strings

--- tests/player_info.py
+++ tests/player_info.py
@@ -11,31 +11,31 @@ if __name__ == '__main__':
     clan = rs3.Clan(player.clan, set_exp=True)
 
     # Prints the player name, real case-sensitive name if the user has his Runemetrics profile not Private, otherwise as passed when setting oject
-    print("Player Name:", player.name)
+    print(f"Player Name: {player.name}")
 
     # Prints some player info in Dictionary format
-    print("Player Info:", player.info)
+    print(f"Player Info: {player.info}")
 
     # Printing "player"'s Clan
-    print("Player Clan:", player.clan)
+    print(f"Player Clan: {player.clan}")
 
     # Printing player's total EXP (If his Runemetrics profile is private, value will always be 0)
-    print("Player Total Exp:", player.exp)
+    print(f"Player Total Exp: {player.exp}")
 
     # Printing the total exp of "clan"
-    print("Clan Exp:", clan.exp)
+    print(f"Clan Exp: {clan.exp}")
 
     # Printing info in Dictionary format of the "clan"'s member "Pedim" (case-sensitive)
-    print("Clan info of 'Pedim':", clan.member['Pedim'])
+    print(f"Clan info of 'Pedim': {clan.member['Pedim']}")
 
     # Printing the 'rank' of "Pedim" in his clan
-    print("Rank of 'Pedim':", clan.member['Pedim']['rank'])
+    print(f"Rank of 'Pedim': {clan.member['Pedim']['rank']}")
 
     # Printing the player count of clan
-    print("Player Count of clan:", clan.count)
+    print(f"Player Count of clan: {clan.count}")
 
     # Printing the average clan exp per member of clan
-    print("Average Clan Exp per member:", clan.avg_exp)
+    print(f"Average Clan Exp per member: {clan.avg_exp}")
 
     # An example of handling exceptions for non-existent clans
     try:
