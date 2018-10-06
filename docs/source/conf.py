616b553fb18ccd825f3ee0740818a4897bb9f039
John Victor <johnvictorfs@gmail.com>
2018-10-06 14:34:27 -0300

    Updated players docstring and docs to reflect changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,8 +73,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-# pygments_style = 'sphinx'
-pygments_style = 'numpy'
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
--- rs3clans/players.py
+++ rs3clans/players.py
@@ -255,7 +255,7 @@ class Player:
         ----------
         Skill object
             Attributes of Skill
-            ----------
+            --------------------
                 exp (or 'xp') : Total exp the Player has in that skill
 
                 level : The level the Player has in that skill
