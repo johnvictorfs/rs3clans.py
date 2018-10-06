f7b9c54ac0c728a3b21f870842d93d8b9f92e2f9
John Victor <johnvictorfs@gmail.com>
2018-10-06 13:18:27 -0300

    Added unit tests for players and clans modules

---
+++ tests/test_clans.py
@@ -0,0 +1,71 @@
+import pytest
+from rs3clans import clans
+from rs3clans import ClanNotFoundError
+import requests
+import csv
+
+
+CLAN_NAME = 'Atlantis'
+
+
+def get_clan_list(clan_name):
+    clan_url = f'http://services.runescape.com/m=clan-hiscores/members_lite.ws?clanName={clan_name}'
+    with requests.Session() as session:
+        download = session.get(clan_url)
+        decoded = download.content.decode('windows-1252')
+        clan_list = list(csv.reader(decoded.splitlines(), delimiter=','))
+        if clan_list[0][0] != "Clanmate":
+            raise ClanNotFoundError(f"Couldn't find clan: {clan_name}")
+        for row in clan_list:
+            row[0] = row[0].replace(r"\xa0", " ")
+    return clan_list
+
+
+def get_clan_exp(clan_list):
+    return sum(int(row[2]) for row in clan_list[1:])
+
+
+@pytest.fixture
+def valid_clan():
+    valid_clan = clans.Clan(CLAN_NAME, set_exp=True)
+    return valid_clan
+
+
+@pytest.fixture
+def valid_clan_list():
+    return get_clan_list(CLAN_NAME)
+
+
+@pytest.fixture
+def valid_clan_exp(valid_clan_list):
+    return get_clan_exp(valid_clan_list)
+
+
+def test_attribute_types(valid_clan, valid_clan_list):
+    assert isinstance(valid_clan.name, str)
+    assert isinstance(valid_clan.exp, int)
+    assert isinstance(valid_clan.member, dict)
+    assert isinstance(valid_clan.avg_exp, float) or isinstance(valid_clan_exp.avg_exp, int)
+    assert isinstance(valid_clan.count, int)
+    assert isinstance(valid_clan.get_member(valid_clan_list[1][0]), dict)
+
+
+def test_clan_name(valid_clan):
+    assert valid_clan.name == CLAN_NAME
+
+
+def test_clan_exception():
+    with pytest.raises(ClanNotFoundError):
+        clans.Clan('clan_that_cannot_possibly_exist_999999')
+
+
+def test_clan_exp(valid_clan, valid_clan_exp):
+    assert valid_clan.exp == valid_clan_exp
+
+
+def test_clan_leader(valid_clan, valid_clan_list):
+    valid_clan_leader = None
+    for key, value in valid_clan.member.items():
+        if value['rank'] == 'Owner':
+            valid_clan_leader = key
+    assert valid_clan_list[1][0] == valid_clan_leader
---
+++ tests/test_players.py
@@ -0,0 +1,85 @@
+import pytest
+from rs3clans import players
+
+"""
+Test Account created for the purpose of those tests:
+
+In-game Name: pyTestAcc
+Clan: None
+Total Exp: 1_254
+Private Profile: False
+Woodcutting Exp: 100
+Woodcutting Level: 2
+Constitution Exp: 1_154
+"""
+
+
+@pytest.fixture
+def valid_player():
+    valid_player = players.Player('pytestacc')
+    return valid_player
+
+
+def test_player_doesnt_exist():
+    player = players.Player('non_existent_player_99999')
+    print(player.exists)
+    assert not player.exists
+
+
+def test_player_clan(valid_player):
+    assert not valid_player.clan
+
+
+def test_player_exists(valid_player):
+    assert valid_player.exists
+
+
+def test_player_name(valid_player):
+    assert valid_player.name == 'pyTestAcc'
+
+
+def test_player_exp(valid_player):
+    assert valid_player.exp == 1_254
+
+
+def test_attribute_types(valid_player):
+    assert isinstance(valid_player.exp, int)
+    assert isinstance(valid_player.skill_values, list)
+    assert isinstance(valid_player.exists, bool)
+    assert isinstance(valid_player.title, str)
+
+
+@pytest.mark.parametrize("skill, result",
+                         [
+                             ('woodcutting', 2),
+                             ('wOoDcutTing', 2),
+                             ('atTaCk', 1),
+                             ('attack', 1),
+                             ('ConstItuTion', 10),
+                             ('constitution', 10),
+                             (3, 10),
+                             (8, 2),
+                             (0, 1),
+                             (26, 1),
+                             (13, 1),
+                         ])
+def test_skill_level(valid_player, skill, result):
+    assert valid_player.skill(skill).level == result
+
+
+@pytest.mark.parametrize("skill, result",
+                         [
+                             ('woodcutting', 100),
+                             ('WoOdcuttIng', 100),
+                             ('constitution', 1_154),
+                             ('CoNsTiTutioN', 1_154),
+                             ('attack', 0),
+                             ('aTTacK', 0),
+                             (3, 1_154),
+                             (8, 100),
+                             (0, 0),
+                             (26, 0),
+                             (13, 0),
+                         ])
+def test_skill_exp(valid_player, skill, result):
+    assert valid_player.skill(skill).exp == result
